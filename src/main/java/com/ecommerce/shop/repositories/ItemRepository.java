6fc5bf8e403ec1f640b6ad9828bfa13d7043b2ed
acriste <cristeandreiflorin@gmail.com>
2025-12-04 11:15:58 +0200

    added metrics on repo method

--- src/main/java/com/ecommerce/shop/repositories/ItemRepository.java
+++ src/main/java/com/ecommerce/shop/repositories/ItemRepository.java
@@ -1,8 +1,15 @@
 package com.ecommerce.shop.repositories;
 
 import com.ecommerce.shop.entities.ItemEntity;
+import io.micrometer.observation.annotation.Observed;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ItemRepository extends CrudRepository<ItemEntity, Long> {
+
+    @Observed(
+            name = "com.ecommerce.shop.repositories.ItemRepository.findById", // The base name for the metric and span
+            contextualName = "findById", // A more descriptive name for the trace span
+            lowCardinalityKeyValues = {"item"} // Tags for metrics and traces
+    )
     ItemEntity findById(long id);
 }
